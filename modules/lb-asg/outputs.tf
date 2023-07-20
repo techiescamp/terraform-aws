93b232987236e82e57e14275b91077e0d10b1a3c
Aswin1118 <aswinvj390@gmail.com>
2023-07-20 15:16:53 +0530

    add variables for lb-asg

---
+++ modules/lb-asg/outputs.tf
@@ -0,0 +1,14 @@
+output "load_balancer_dns_name" {
+  description = "LoadBalancer dns name"
+  value = aws_lb.application_lb.dns_name
+}
+
+output "auto_scaling_group_name" {
+  description = "Auto scaling group name"
+  value = aws_autoscaling_group.application_asg.name
+}
+
+output "launch_template_id" {
+  description = " launch template id"
+  value = aws_launch_template.application_lt.id
+}
