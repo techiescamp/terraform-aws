993520bbd3d531575c4336aede9cb6328e6252c1
Aswin1118 <aswinvj390@gmail.com>
2023-08-02 20:34:03 +0530

    test

--- environments/dev/alb-asg/main.tf
+++ environments/dev/alb-asg/main.tf
@@ -14,19 +14,37 @@ module "alb-sg" {
   source                          = "../../../modules/security-group"
   region                          = var.region
   tags                            = var.tags
+  name                            = "${var.environment}-${var.application}"
   environment                     = var.environment
   owner                           = var.owner
   cost_center                     = var.cost_center
   application                     = var.application
+  sg_name                         = var.alb_sg_name
   vpc_id                          = var.vpc_id
-  ingress_from_port               = var.alb_ingress_from_port
-  ingress_to_port                 = var.alb_ingress_to_port
-  ingress_protocol                = var.alb_ingress_protocol
+
+  ingress_cidr_from_port          = var.alb_ingress_cidr_from_port
+  ingress_cidr_to_port            = var.alb_ingress_cidr_to_port
+  ingress_cidr_protocol           = var.ingress_cidr_protocol
   ingress_cidr_block              = var.alb_ingress_cidr_block
-  egress_from_port                = var.alb_egress_from_port
-  egress_to_port                  = var.alb_egress_to_port
-  egress_protocol                 = var.alb_egress_protocol
+  create_ingress_cidr             = var.alb_create_ingress_cidr
+
+  ingress_sg_from_port            = var.alb_ingress_sg_from_port
+  ingress_sg_to_port              = var.alb_ingress_sg_to_port
+  ingress_sg_protocol             = var.alb_ingress_sg_protocol
+  ingress_security_group_ids      = var.security_group_id
+  create_ingress_sg               = var.alb_create_ingress_sg
+
+  egress_cidr_from_port           = var.alb_egress_cidr_from_port
+  egress_cidr_to_port             = var.alb_egress_cidr_to_port
+  egress_cidr_protocol            = var.alb_egress_cidr_protocol
   egress_cidr_block               = var.alb_egress_cidr_block
+  create_egress_cidr              = var.alb_create_egress_cidr
+
+  egress_sg_from_port             = var.alb_egress_sg_from_port
+  egress_sg_to_port               = var.alb_egress_sg_to_port
+  egress_sg_protocol              = var.alb_egress_sg_protocol
+  egress_security_group_ids       = var.security_group_id
+  create_egress_sg                = var.alb_create_egress_sg
 }
 
 module "alb" {
@@ -57,17 +75,46 @@ module "alb" {
   security_group_ids              = module.alb-sg.security_group_ids
 }
 
+module "instance-sg" {
+  source                          = "../../../modules/security-group"
+  region                          = var.region
+  tags                            = var.tags
+  name                            = "${var.environment}-${var.application}"
+  environment                     = var.environment
+  owner                           = var.owner
+  cost_center                     = var.cost_center
+  application                     = var.application
+  sg_name                         = var.sg_name
+  vpc_id                          = var.vpc_id
+
+  ingress_cidr_from_port          = var.ingress_cidr_from_port
+  ingress_cidr_to_port            = var.ingress_cidr_to_port
+  ingress_cidr_protocol           = var.ingress_cidr_protocol
+  ingress_cidr_block              = var.ingress_cidr_block
+  create_ingress_cidr             = var.create_ingress_cidr
+
+  ingress_sg_from_port            = var.ingress_sg_from_port
+  ingress_sg_to_port              = var.ingress_sg_to_port
+  ingress_sg_protocol             = var.ingress_sg_protocol
+  ingress_security_group_ids      = module.alb-sg.security_group_ids
+  create_ingress_sg               = var.create_ingress_sg
+
+  egress_cidr_from_port           = var.egress_cidr_from_port
+  egress_cidr_to_port             = var.egress_cidr_to_port
+  egress_cidr_protocol            = var.egress_cidr_protocol
+  egress_cidr_block               = var.egress_cidr_block
+  create_egress_cidr              = var.create_egress_cidr
+
+  egress_sg_from_port             = var.egress_sg_from_port
+  egress_sg_to_port               = var.egress_sg_to_port
+  egress_sg_protocol              = var.egress_sg_protocol
+  egress_security_group_ids       = module.alb-sg.security_group_ids
+  create_egress_sg                = var.create_egress_sg
+}
+
 module "asg" {
   source                          = "../../../modules/asg"
   ami_id                          = var.ami_id
-  ingress_from_port               = var.ingress_from_port
-  ingress_to_port                 = var.ingress_to_port
-  ingress_protocol                = var.ingress_protocol
-  ingress_cidr_block              = var.ingress_cidr_block
-  egress_from_port                = var.egress_from_port
-  egress_to_port                  = var.egress_to_port
-  egress_protocol                 = var.egress_protocol
-  egress_cidr_block               = var.egress_cidr_block
   instance_type                   = var.instance_type
   key_name                        = var.key_name
   vpc_id                          = var.vpc_id
@@ -84,7 +131,7 @@ module "asg" {
   application                     = var.application
   alb_target_group_arn            = module.alb.alb_target_group_arn
   iam_role                        = module.iam-policy.iam_role
-  security_group_ids              = module.alb-sg.security_group_ids
+  security_group_ids              = module.instance-sg.security_group_ids
   tags = {
     Owner                         = "${var.owner}"
     Environment                   = "${var.environment}"
