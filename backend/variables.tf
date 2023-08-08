2c09e779b4e5f51a819520a3b047523b5e7bb383
Aswin1118 <aswinvj390@gmail.com>
2023-08-08 19:41:04 +0530

    Add s3 and dynamodb table provisioning code

--- backend/variables.tf
+++ backend/variables.tf
@@ -1,17 +1,11 @@
 variable "bucket_name" {
-  default = "testy2137237-bucket"
+  default = "petclinic-statelock-bucket"
   description = "Name of the S3 bucket for Terraform state storage"
   type        = string
 }
 
 variable "table_name" {
-  default = "test-table"
+  default = "petclinic-statelock-table"
   description = "Name of the DynamoDB table for Terraform state locking"
-  type        = string
-}
-
-variable "key" {
-  default = "test/statefile.tfstate"
-  description = "Path of the state file"
   type        = string
 }
