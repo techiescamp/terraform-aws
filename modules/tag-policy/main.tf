9f8b0e9946d32f8721a5894f213019cbbbf77bdb
Administrator <admin@example.com>
2023-08-09 22:40:09 +0530

    [TEC-69] Update tag-policy module to enforce specific values

--- modules/tag-policy/main.tf
+++ modules/tag-policy/main.tf
@@ -12,25 +12,25 @@ resource "aws_organizations_policy" "tag_policy" {
     "tags" = {
       "Name" = {
         "tag_key"      = { "@@assign" = var.name_tag_key },
-        "enforced_for" = { "@@assign" = var.name_enforce_for_values }
+        "enforced_for" = { "@@assign" = var.enforce_for_values }
       },
       "Environment" = {
         "tag_key"      = { "@@assign" = var.environment_tag_key },
-        "enforced_for" = { "@@assign" = var.environment_enforce_for_values }
+        "enforced_for" = { "@@assign" = var.enforce_for_values }
       },
       "Owner" = {
         "tag_key"      = { "@@assign" = var.owner_tag_key },
         "tag_value"    = { "@@assign" = var.owner_tag_value },
-        "enforced_for" = { "@@assign" = var.owner_enforce_for_values }
+        "enforced_for" = { "@@assign" = var.enforce_for_values }
       },
       "CostCenter" = {
         "tag_key"      = { "@@assign" = var.costcenter_tag_key },
         "tag_value"    = { "@@assign" = var.costcenter_tag_value },
-        "enforced_for" = { "@@assign" = var.costcenter_enforce_for_values }
+        "enforced_for" = { "@@assign" = var.enforce_for_values }
       },
       "Application" = {
         "tag_key"      = { "@@assign" = var.application_tag_key },
-        "enforced_for" = { "@@assign" = var.application_enforce_for_values }
+        "enforced_for" = { "@@assign" = var.enforce_for_values }
       }
     }
   })
